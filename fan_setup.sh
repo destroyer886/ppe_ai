e5daf3acedc496f5a98733ec3a5a4d2b489c8695
ThunderCoder <134991440+destroyer886@users.noreply.github.com>
2026-02-28 23:57:00 +0530

    Create fan_setup.sh

---
+++ fan_setup.sh
@@ -0,0 +1,15 @@
+Systemd pe ye chalana hai 
+
+cat << 'EOF' > /usr/local/bin/jetson_init.sh
+#!/bin/bash
+# Set Fan to Max
+if [ -e /sys/devices/pwm-fan/target_pwm ]; then
+    echo 255 > /sys/devices/pwm-fan/target_pwm
+fi
+
+# Set Static IP (Replace eth0 with your interface name)
+# nmcli con mod "Wired connection 1" ipv4.addresses 192.168.1.100/24 ipv4.method manual
+# nmcli con up "Wired connection 1"
+EOF
+
+chmod +x /usr/local/bin/jetson_init.sh
